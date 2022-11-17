486ebc20dd17d23d8385b228f79c4f99f8469a7b
ThatBroseph159 <109487079+ThatBroseph159@users.noreply.github.com>
2022-11-17 11:17:04 -0600

    Update GameFunctions.h

--- GameFunctions.h
+++ GameFunctions.h
@@ -1,5 +1,7 @@
 void getNumberOfWords(int* NUM_WORDS, FILE* wordsList);
+void getMinimumWords(int* MinimumWordPlay);
 void startUpFunction(bool validStartUp, char startUpButton, bool* playMode, bool* writeMode);
+void fileScan(char* strWordList[], int strLength[]);
 void startUpPromot(int NUM_WORDS);
 void appendFunc(void);
 void drawGameBorder(void);
