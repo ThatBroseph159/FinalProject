c2658dfb54254a2bd431b24f074cad09a6958662
ClappedYourCake <109487079+ThatBroseph159@users.noreply.github.com>
2022-11-29 13:48:40 -0600

    Update GameFunctions.h

--- GameFunctions.h
+++ GameFunctions.h
@@ -1,8 +1,15 @@
+typedef struct word_struct{
+    char word[20];
+    int xPos;
+    int yPos;
+} wordInfoList;
+
 void getNumberOfWords(int* NUM_WORDS, FILE* wordsList);
 void getMinimumWords(int* MinimumWordPlay);
 void startUpFunction(bool validStartUp, char startUpButton, bool* playMode, bool* writeMode);
 void fileScan(char* strWordList[], int strLength[]);
-void startUpPromot(int NUM_WORDS);
+void startUpPrompt(int NUM_WORDS);
 void appendFunc(void);
 void drawGameBorder(void);
-void addWord(char* WL[], int NUM_WORDS, int* x, int* y, int MinimumWordPlay);
+void addWord(char* WL[], int NUM_WORDS, int MinimumWordPlay, wordInfoList gameWords[], int* wordArray);
+void moveWords(wordInfoList gameWords[], int* wordArray);
